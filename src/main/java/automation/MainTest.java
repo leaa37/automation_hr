9cbef6713e4cee9b64b37999df146545efe3eba0
Leaa <42497423+leaa37@users.noreply.github.com>
2021-03-08 17:09:14 -0300

    Refactor/base (#1)
    * Improve setup and configuration
    
    * Adding Log Helper and custom logs
    
    Co-authored-by: Leandro Nicolás García <lngarcia@baufest.com>

--- src/main/java/automation/MainTest.java
+++ src/main/java/automation/MainTest.java
@@ -2,19 +2,40 @@ package automation;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+
+import scripts.login.Login;
 
 public class MainTest {
 
 	private WebDriver driver;
+	private WebDriverHelper browser;
+	private LogHelper logger;
 
 	@Before
 	public void beforeTest() {
-		driver = new WebDriverHelper().generateWebDriver();
+		// Initialize
+		logger = new LogHelper();
+		browser = new WebDriverHelper();
+		driver = browser.generateWebDriver();
+
+		logger.log("Driver initialized successfully");
+	}
+
+	@Test
+	public void mainTest() {
+		// Run tests
+		logger.log("Starting running tests...");
+
+		// To Do
 	}
 
 	@After
 	public void afterTest() {
-		driver.quit();
+		if (driver != null) {
+			driver.quit();
+		}
+		logger.log("Tests execution finished");
 	}
 }
