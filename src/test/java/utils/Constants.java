f50b5f7af63937947338c9d8313fca38af6b115d
Leaa <42497423+leaa37@users.noreply.github.com>
2021-03-09 12:29:02 -0300

    Password changed

--- src/test/java/utils/Constants.java
+++ src/test/java/utils/Constants.java
@@ -3,7 +3,7 @@ package utils;
 public class Constants {
 	public static final String mainURL = "https://www.mulesoft.com/";
 	public static final String loginUserName = "leang07";
-	public static final String loginPassword = "T730sh32";
+	public static final String loginPassword = "Lhkm3akk5";
 	public static final String profileTextLogo = "LG";
 	public static final String profileFullName = "Leandro Garcia";
 	public static final String downloadedText = "Thank you for downloading Anypoint Studio and Mule";
